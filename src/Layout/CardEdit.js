267362e3c18be555eebf075ad2a153b48b5c9619
Leana Lopez <chileana_go@yahoo.com>
2021-07-25 15:53:59 -0700

    added parseInt on deckId

--- src/Layout/CardEdit.js
+++ src/Layout/CardEdit.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { readDeck, readCard , updateCard } from "../utils/api/index";
 import Breadcrumb from "./Breadcrumb";
+import CardForm from "./CardForm";
 
 export default function CardEdit() {
   const { deckId } = useParams();
   const { cardId } = useParams();
-  const history = useHistory();
   const [ deck, setDeck ] = useState({});
   const [ card, setCard ] = useState({});
   const [ front, setFront ] = useState("");
@@ -40,20 +40,19 @@ export default function CardEdit() {
 
   const frontChangeHandler = (event) => setFront(event.target.value);
   const backChangeHandler = (event) => setBack(event.target.value);
-  const cancelClickHandler = () => history.push(`/decks/${deckId}`);
 
-  async function submitHandler() {
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    
     const abortController = new AbortController();
     const updatedCard = {
         id: cardId,
+        deckId: parseInt(deckId),
         front: front,
         back: back
-    }
-    const apiUpdate = await updateCard(updatedCard, abortController.signal)
-    console.log("apiUpdate",apiUpdate)
-    // setCard(apiUpdate)
-    // setFront(apiUpdate.front);
-    // setBack(apiUpdate.back);
+    };
+    await updateCard(updatedCard, abortController.signal);
+ 
     return () => abortController.abort();
   };
 
@@ -61,36 +60,16 @@ export default function CardEdit() {
     <div>
       <Breadcrumb deckId={deckId} deckName={deck.name} addText={`Edit Card ${card.id}`} />
       <h2 className="card-title">Edit Card</h2>
-      <form name="add" onSubmit={submitHandler}>
-        <div className="form-group">
-          <label htmlFor="front">Front</label>
-          <textarea
-            className="form-control"
-            id="front" 
-            name="front" 
-            placeholder={card.front}
-            required={true} 
-            rows={2} 
-            onChange={frontChangeHandler}
-            value={front}
-          ></textarea>
-        </div>
-        <div className="form-group">
-          <label htmlFor="back">Back</label>
-          <textarea
-            className="form-control"
-            id="back" 
-            name="back" 
-            placeholder={card.back}
-            required={true} 
-            rows={2} 
-            onChange={backChangeHandler}
-            value={back}
-          ></textarea>
-        </div>
-        <button type="button" className="btn btn-secondary" onClick={cancelClickHandler}>Cancel</button><span>  </span>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+      <CardForm 
+        deckId={deckId} 
+        frontPlaceholder={card.front} 
+        backPlaceholder={card.back}
+        front={front}
+        back={back}
+        frontChangeHandler={frontChangeHandler}
+        backChangeHandler={backChangeHandler} 
+        submitHandler={submitHandler}
+      />
     </div>
   );
 }
