5d39ab3fb9eed92ee3daf24ba6f7d086dbd19f98
Leana Lopez <chileana_go@yahoo.com>
2021-07-25 15:54:56 -0700

    re-designed Deckname for qualified test purposes

--- src/Layout/DeckList.js
+++ src/Layout/DeckList.js
@@ -1,54 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { listDecks } from "../utils/api/index";
 import { deleteDeck } from "../utils/api/index";
-import { Link , useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export default function DeckList() {
-    const [decks, setDecks] = useState([]);
-    const history = useHistory();
+  const [decks, setDecks] = useState([]);
+  const history = useHistory();
 
-    useEffect(() => {
-        const abortController = new AbortController();
+  useEffect(() => {
+    const abortController = new AbortController();
 
-        async function loadDecks() {
-            const decksFromApi = await listDecks(abortController.signal)
-            setDecks(decksFromApi);
-        }
+    async function loadDecks() {
+      const decksFromApi = await listDecks(abortController.signal);
+      setDecks(decksFromApi);
+    }
 
-        loadDecks();
+    loadDecks();
 
-        return () => abortController.abort();
-    }, []);
+    return () => abortController.abort();
+  }, []);
 
-    if (decks === []) return "Empty decks.";
+  if (decks === []) return "Empty decks.";
 
-    function deleteClickHandler( deckId ) {
-        const confirmDeletion = window.confirm("\nDelete this deck?\n\nYou will not be able to recover it.");
+  function deleteClickHandler(deckId) {
+    const confirmDeletion = window.confirm(
+      "\nDelete this deck?\n\nYou will not be able to recover it."
+    );
 
-        if (!confirmDeletion) return history.push("/");
+    if (!confirmDeletion) return history.push("/");
 
-        const abortController = new AbortController();
+    const abortController = new AbortController();
 
-        async function deleteThisDeck(){
-            await deleteDeck(deckId, abortController.signal);
-        }
+    async function deleteThisDeck() {
+      await deleteDeck(deckId, abortController.signal);
+    }
 
-        deleteThisDeck();
+    deleteThisDeck();
 
-        window.location.reload();
-    };
+    window.location.reload();
+  }
 
-    return decks.map((deck) => {
-        return (
-        <div key={deck.id} className="card">
-            <div className="card-body">
-                <h2 className="card-title">{deck.name} - {deck.cards.length} cards</h2>
-                <p className="card-text">{deck.description}</p>
-                <Link to={`/decks/${deck.id}`} className="btn btn-secondary">View</Link><span>  </span>
-                <Link to={`/decks/${deck.id}/study`} className="btn btn-primary">Study</Link><span>  </span>
-                <button type="button" className="btn btn-danger" onClick={() => deleteClickHandler(deck.id)}>Delete</button>
+  return decks.map((deck) => {
+    return (
+      <div key={deck.id} className="card">
+        <div className="card-body">
+          <div className="row">
+            <div className="col">
+              <h2 className="card-title">{deck.name}</h2>
             </div>
+            <div className="col">
+              <h5 className="card-title">{deck.cards.length} cards</h5>
+            </div>
+          </div>
+          <p className="card-text">{deck.description}</p>
+          <Link to={`/decks/${deck.id}`} className="btn btn-secondary">
+            View
+          </Link>
+          <span> </span>
+          <Link to={`/decks/${deck.id}/study`} className="btn btn-primary">
+            Study
+          </Link>
+          <span> </span>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => deleteClickHandler(deck.id)}
+          >
+            Delete
+          </button>
         </div>
-        );
-    });
+      </div>
+    );
+  });
 }
